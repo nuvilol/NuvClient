09c1236526ce8370a7fe9ad1eb61bebcc1f351cb
nuvilol <justconnor13@gmail.com>
2025-05-04 21:58:06 -0700

    Update ExpandHitbox.lua

--- Files/ExpandHitbox.lua
+++ Files/ExpandHitbox.lua
@@ -1,7 +1,1 @@
-for _, v in ipairs(plrs) do
-    if v.Character:FindFirstChild("Hitbox") or v.Character:FindFirstChild("HitBox") then
-        print("Hitbox found")
-    else
-        print("Hitbox not found")
-    end
-end
+
